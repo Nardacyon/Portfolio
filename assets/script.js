78586f9a35f080720ee65ab247b61c5cb8a2413f
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-11-17 19:23:13 -0800

    made changes to styling and images for portfolio

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-    $('.toggle').click(function() {
-        $('.toggle').toggleClass('active')
+    $('.menu li a').click(function() {
+        $('.menu li a').removeClass("active");
+        $(this).toggleClass('active');
 
-        $('nav ul').toggleClass('active-menu')
+        // $('nav ul').toggleClass('active-menu');
+        
     })
 })
